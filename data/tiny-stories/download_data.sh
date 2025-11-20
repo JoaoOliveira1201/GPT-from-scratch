f25436e6f3d5115ebeb1db57c3ed45e8c577bfad
JoaoOliveira1201 <140345492+JoaoOliveira1201@users.noreply.github.com>
2025-11-20 09:56:36 +0000

    Add tiny stories dataset

---
+++ data/tiny-stories/download_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -L -o tinystories-narrative-classification.zip\
+  https://www.kaggle.com/api/v1/datasets/download/thedevastator/tinystories-narrative-classification
+unzip tinystories-narrative-classification.zip
+rm tinystories-narrative-classification.zip
